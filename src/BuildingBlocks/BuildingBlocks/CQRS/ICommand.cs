6df0f1f1cc98234141601b4f004340872ac365a3
guilherme <tadeu.guilherme2002@gmail.com>
2024-04-07 21:57:36 -0300

    feat: add command and query abstraction

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IComand : ICommand<Unit> // Basially an empty interface - Does not return anything
+    {
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse> // It returns a response of type TResponse. Its specified in the part "out TResponse"
+    {
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQuery<out TResponse> : IRequest<TResponse>
+        where TResponse : notnull // This means that TResponse cannot be null. TResponse being in the "out" position means that it can only be returned from the method, not passed in.
+    {
+    }
+}
