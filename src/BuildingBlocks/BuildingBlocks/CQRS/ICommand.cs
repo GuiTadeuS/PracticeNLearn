a9cc3b51994069b6217d51fdf43db1594a4471c7
guilherme <tadeu.guilherme2002@gmail.com>
2024-04-07 22:17:59 -0300

    feat: add cqrs abstractions with mediatr

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -2,10 +2,10 @@
 
 namespace BuildingBlocks.CQRS
 {
-    public interface IComand : ICommand<Unit> // Basially an empty interface - Does not return anything
+    public interface ICommand : ICommand<Unit> // Basically an empty interface - Does not return anything
     {
     }
-    public interface ICommand<out TResponse> : IRequest<TResponse> // It returns a response of type TResponse. Its specified in the part "out TResponse"
+    public interface ICommand<out TResponse> : IRequest<TResponse> // It returns a response of type TResponse. Its specified in the part "out TResponse
     {
     }
 }
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    // If it doesn't return anything, it will return Unit (void)
+    public interface ICommandHandler<in TCommand>
+        : ICommandHandler<TCommand, Unit> 
+        where TCommand : ICommand<Unit> 
+    { 
+    }
+
+    // If it returns something, it will return TResponse
+    public interface ICommandHandler<in TCommand, TResponse> 
+        : IRequestHandler<TCommand, TResponse> 
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+        // This all means that ICommandHandler is a generic interface that takes in a TCommand and TResponse. The where clause specifies that TCommand must be of type ICommand<TResponse> and TResponse must be of type notnull
+    {
+    }
+}
