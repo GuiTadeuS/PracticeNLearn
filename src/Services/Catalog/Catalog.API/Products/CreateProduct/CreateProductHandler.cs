e470391797d442ce81140bd666efbc3a475490bf
guilherme <tadeu.guilherme2002@gmail.com>
2024-04-07 22:22:33 -0300

    refactor: add command and handler abstractions

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,13 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
-    // DTO
-    public record CreateProductCommand(string Name, List<string> Categories, string Description, decimal Price, string ImageFile) : IRequest<CreateProductResult> ;
-    // Record because it's immutable - DTO
+    public record CreateProductCommand(string Name, List<string> Categories, string Description, decimal Price, string ImageFile) 
+        : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    // Application Layer (Use Cases)
-    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     { 
         public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
